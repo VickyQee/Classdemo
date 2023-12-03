008692afcd0b843a2161d7e4617393a3b644f9fc
vickyQeeclear <chukwuv99@gmail.com>
2023-12-04 00:30:44 +0100

    first commit

--- src/main/java/com/example/Classdemo/controller/JavaStudentService.java
+++ src/main/java/com/example/Classdemo/controller/JavaStudentService.java
@@ -7,9 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
